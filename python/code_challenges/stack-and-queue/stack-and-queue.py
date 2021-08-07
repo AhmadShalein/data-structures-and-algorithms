c58e8f25a907fb909ec244e57ac99d7946eab71c
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-08-07 15:16:51 +0300

    Update stack-and-queue.py

--- python/code_challenges/stack-and-queue/stack-and-queue.py
+++ python/code_challenges/stack-and-queue/stack-and-queue.py
@@ -1,43 +1,49 @@
 class Node:
   def __init__(self , value=None):
-    self.value= value
-    self.next=None
+    self.value = value
+    self.next = None
 
 class Stack:
   def __init__(self,node=None):
-    self.top=node
+    self.top = node
   
   def push(self, value):
-    node= Node(value)
+    node = Node(value)
     node.next = self.top
     self.top = node
 
   def pop(self):
-    temp=self.top
-    self.top=self.top.next
-    temp.next=None
+    temp = self.top
+    self.top = self.top.next
+    temp.next = None
     return temp.value
   
   def peek(self):
-    if not self.isEmpty():
-      return self.top.data
-    raise EmptyStackException("stack is empty")
+    if not self.is_empty():
+      return self.top.value
  
   def is_empty(self):
     return not self.top    
-    
+  
 class Queue:
   def __init__(self):
     self.front = None
     self.rear = None
 
   def enqueue(self, value):
-    pass
+    node = Node(value)
+    self.rear.next = node
+    self.rear = self.rear.next
 
   def dequeue(self):
-    pass
+    temp = self.front
+    self.front = temp.next
+    front.next = None
+    return temp.value
+
+  def peek(self):
+    if not self.is_empty():
+      return self.front.value
 
   def is_empty(self):
-    if (not self.rear and self.front) or (self.rear and not self.front):
-      raise Exception("Something is fishy here")
-    return not self.rear
+    return not self.front
