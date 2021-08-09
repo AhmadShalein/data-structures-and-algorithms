0c0e5e8ef92cdd1e455e15b5c57e4c322ee28a48
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-08-09 18:32:31 +0300

    Create stack-queue-animal-shelter.py

---
+++ python/code_challenges/stack-queue-animal-shelter/stack-queue-animal-shelter.py
@@ -0,0 +1,28 @@
+class AnimalShelter():
+    def __init__(self):
+        self.cat = Queue()
+        self.dog = Queue()
+
+    def enqueue(self, animal):
+        if animal.kind == "cat"
+          self.cat.enqueue(animal.nickname)
+        elif animal.kind == "dog"
+          self.dog.enqueue(animal.nickname)
+      
+    def dequeue(self, pref):
+        if pref == "cat"
+          self.cat.dequeue()
+        elif pref == "dog"
+          self.dog.dequeue()
+        else:
+          return "null"
+        
+class Cat():
+  def __init__(self,nickname):
+    self.nickname = nickname
+    self.kind = "cat"
+    
+class Dog():
+  def __init__(self,nickname):
+    self.nickname = nickname
+    self.kind = "dog"
