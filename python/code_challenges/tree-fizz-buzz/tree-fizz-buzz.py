04f4ea236d832952cdb1352e78392c3e31fd1d15
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-08-18 00:51:50 +0300

    Update tree-fizz-buzz.py

--- python/code_challenges/tree-fizz-buzz/tree-fizz-buzz.py
+++ python/code_challenges/tree-fizz-buzz/tree-fizz-buzz.py
@@ -65,7 +65,7 @@ def FizzBuzzTree(tree):
 if __name__ == "__main__":
     bt = binary_tree()
     bt.root = Node(12)
-    bt.root.left = Node(7)
+    bt.root.left = Node(15)
     bt.root.right = Node(5)
     bt.root.left.right = Node(6)
     bt.root.left.left = Node(8)
