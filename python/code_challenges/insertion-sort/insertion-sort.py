92f60381d64caef11d3c9f9c90fbb5168f195cd8
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-08-29 17:17:50 +0300

    Update insertion-sort.py

--- python/code_challenges/insertion-sort/insertion-sort.py
+++ python/code_challenges/insertion-sort/insertion-sort.py
@@ -1,1 +1,9 @@
-Insertion Sort
+def insertion_sort(arr):
+    for i in range (1, len(arr)):
+        j = i-1
+        key = list [i]
+        while j >= 0 and key < arr[j]:
+            list[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key   
+    return arr
