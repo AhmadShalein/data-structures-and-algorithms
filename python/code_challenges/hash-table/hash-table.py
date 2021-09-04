a6fe12a7c87e6bd59945db0ff9e3e540ddcc8fe2
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-09-04 18:31:41 +0300

    Update hash-table.py

--- python/code_challenges/hash-table/hash-table.py
+++ python/code_challenges/hash-table/hash-table.py
@@ -1,73 +1,60 @@
-class Node():
-  def __init__(self, value=None):
-    self.value = value
-    self.next = None
+class HashTable():
+  def __init__(self ,size = 16):
+    """initialization hash table"""
+    self.max = size #length of list
+    self.arr = [[] for i in range(self.max)] 
+    # or [None]*self.size\ we store key value pairs in each index [('Key',value),('Key',value)]
 
-  def __add__(self, other):
-    return Node(self.value + other.value)
+  def get_hash(self, key):
+    """function to return the hash value using ASCII code"""
+    h = 0
+    for char in key:
+        h += ord(char)
+    hash_index= h % self.max 
+    return hash_index
+ 
+  def add(self ,key ,value):
+    """Function the store key value pairs in the key index of list"""
+    h = self.get_hash(key)
+    found = False
+    # we have to loop in the linked list to check if the key is found then update its value 
+    for idx, element in enumerate(self.arr[h]):
+      if len(element)==2 and element[0] == key:
+          self.arr[h][idx] = (key,value)
+          found = True
+    if not found: # if not found store it 
+      self.arr[h].append((key,value))
 
-  def __str__(self):
-    return str(self.value)
+  def get(self, key):
+    """function that return the value stored in the key index"""
+    h = self.get_hash(key)
+    for element in self.arr[h]:
+      if element[0] == key:
+        return element[1] # return the value
 
+  def contains(self, key):
+    h = self.get_hash(key)
+    found = False
+    # we have to loop in the linked list to check if the key is found then update its value 
+    for idx, element in enumerate(self.arr[h]):
+      if len(element)==2 and element[0] == key:
+        found = True
+    return found
 
-class LinkedList():
-  def __init__(self):
-    self.head = None
+  def delete(self , key):
+    """function that set the value of key to None"""
+    h = self.get_hash(key)
+    for idx , element in enumerate(self.arr[h]):
+      if element[0] == key:
+        del self.arr[h][idx]
 
-  def insert(self, value):
-    node = Node(value)
-    if self.head:
-      node.next = self.head
-    self.head = node
-
-  def includes(self,vlaue):
-     current=self.head
-     while current:
-         if vlaue ==current.value[0]:
-            return True
-         current=current.next
-     return False
-
-  def append(self,value):
-    new_node=Node(value)
-    if self.head == None:
-      self.head = new_node
-    else:
-      current=self.head
-      while current.next:
-        current=current.next
-      current.next=new_node
-
-class HashTable:
-
-  def __init__(self, size = 1024):
-    
-    self.size = size
-    self._buckets = [None] *self.size
-
-  def hash(self,key:str)->int:
-    value=0
-    for x in key:
-      value += ord(x)
-    index = (value * 7) % self.size   
-    return index
-
-  def add(self,key,value):
-
-    index = self.hash(key) 
-
-    if not self._buckets[index]:
-      self._buckets[index] = LinkedList()
-    self._buckets[index].append([key,value])
-
-  def find(self,key):
-    index = self.hash[key]
-    return index
-    
-
-  def contains(self,key):
-    index=self.hash(key)
-    if self._buckets[index]:
-      return self._buckets[index].includes(key)
-    else:
-      return False
+if __name__ == "__main__":
+  hash_table = HashTable()
+  hash_table.add('August 1st',10)
+  hash_table.add('August 5th',50)
+  hash_table.add('August 10th',100)
+  hash_table.add('August 15th',150)
+  hash_table.add('August 20th',200)
+  hash_table.add('August 25th',250)
+  for idx in hash_table.arr:
+    print(idx)
