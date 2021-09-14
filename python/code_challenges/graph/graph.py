d7187543b3406c9ad6eb9f34a4abc1e948f92a81
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-09-14 16:19:14 +0300

    Update graph.py

--- python/code_challenges/graph/graph.py
+++ python/code_challenges/graph/graph.py
@@ -1,3 +1,6 @@
+#Lab 35
+from collections import deque
+
 class Vertex():
   def __init__(self,key):
     self.key = key
@@ -7,7 +10,7 @@ class Vertex():
 
 class Queue():
   def __init__(self):
-    self.dq =deque()
+    self.dq = deque()
   
   def enqueue(self,value):
     self.dq.appendleft(value)
@@ -22,15 +25,13 @@ class Graph():
   def __init__(self):
     self._adjacency_list = {}
     
-
-  def add_node(self ,value):
+  def add_node(self,value):
     new_v = Vertex(value)
-    new_v=str(new_v)
+    new_v = str(new_v)
     self._adjacency_list[new_v] = []
     return new_v
     
-
-  def add_edge(self , start_vertex ,end_vertex ,weight =0):
+  def add_edge(self,start_vertex,end_vertex,weight =0):
     if start_vertex not in self._adjacency_list:
       raise KeyError('Start vertex is not found in the graph')
     if end_vertex not in self._adjacency_list:
@@ -40,12 +41,28 @@ class Graph():
   def get_nodes(self):
     return self._adjacency_list.keys()
     
-  
-  def neighbors(self , vertex):
+  def neighbors(self,vertex):
     return self._adjacency_list[vertex]
   
   def size(self):
     return len(self._adjacency_list)
-   
+
+#Lab 36
+  def breath_first_search(self,start_vertex):
+    output=[]
+    queue = Queue()
+    visited = set()
+    queue.enqueue(start_vertex)
+    visited.add(start_vertex)
+    while len(queue):
+      current_vertex = queue.dequeue()
+      output.append(current_vertex)
+      for i in self._adjacency_list[current_vertex]:
+        if i[0] not in visited:
+          i=i[0]
+          visited.add(i)
+          queue.enqueue(i)
+    return output
+
   def print_graph(self):
     print(self._adjacency_list)
